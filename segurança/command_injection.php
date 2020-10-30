688735db4c5ef4c10e18664c6ba73aa07ca879fe
napoleao.onaya@gmail.com <napoleao.onaya@gmail.com>
2020-10-29 21:38:33 -0300

    Command Injection

--- segurança/command_injection.php
+++ segurança/command_injection.php
@@ -1,5 +1,31 @@
 <?php
 
+if($_SERVER["REQUEST_METHOD"] === "POST"){
 
+    $cmd = escapeshellcmd($_POST["cmd"]);
+
+    var_dump($cmd);
+
+    echo "<pre>";
+
+    $comando = system($cmd, $retorno);
+
+    echo "</pre>";
+
+    /*
+    * Atenção para função system e exec sempre trate os dados, usando a função escapeshellcmd().
+    */
+
+}
+
+//echo "<pre>";
+
+//$comando = system("ls -lta",$retorno); Dado estático
+
+//echo "</pre>";
 
 ?>
+<form method="POST">
+    <input type="text" name="cmd"/>
+    <button type="submit">Eviar</button>
+</form>
